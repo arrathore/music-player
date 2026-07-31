8efbbadbcc05e29b9bca9ce7a8ea83611909436b
Apoorav Rathore <apoorav.rathore@icloud.com>
2026-07-30 19:21:00 -0500

    started work on companion app

---
+++ music-manager/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+import webbrowser
+import threading
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI(title="SD music manager")
+
+# serve frontend
+app.mount("/static", StaticFiles(directory="client"), name="static")
+
+# index
+@app.get("/")
+def root():
+    return FileResponse("client/index.html")
+
+# open browser after server starts
+def open_browser():
+    webbrowser.open("http://localhost:8000")
+
+if __name__ == "__main__":
+    # open browser 1 second after server starts
+    timer = threading.Timer(1.0, open_browser)
+    timer.start()
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+
