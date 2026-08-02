90cc322afb1ebdec7c231c9ecba0fdd57b95d850
Apoorav Rathore <apoorav.rathore@icloud.com>
2026-08-02 17:38:31 -0500

    added title bar

--- music-manager/client/music-manager/src/App.tsx
+++ music-manager/client/music-manager/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import TitleBar from "./components/title-bar/TitleBar";
 import ImportPane from "./components/import/ImportPane";
 import SDCardPane from "./components/sdcard/SDCardPane";
 
 function App() {
   return (
     <div className="App">
+      <TitleBar />
       <div id="main">
 	<div className="pane">
 	  <ImportPane />
---
+++ music-manager/client/music-manager/src/components/title-bar/TitleBar.tsx
@@ -0,0 +1,9 @@
+function TitleBar() {
+  return (
+    <div className="TitleBar">
+      <h1>music manager</h1>
+    </div>
+  );
+}
+
+export default TitleBar;
