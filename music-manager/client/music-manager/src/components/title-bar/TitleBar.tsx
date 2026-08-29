09cdeb85b5d8d2022588146e865e0afcbb6b6da4
Apoorav Rathore <apoorav.rathore@icloud.com>
2026-08-29 14:26:17 -0500

    moved music manager to its own repository

--- music-manager/client/music-manager/src/components/title-bar/TitleBar.tsx
+++
@@ -1,9 +0,0 @@
-function TitleBar() {
-  return (
-    <div className="TitleBar">
-      <h1>music manager</h1>
-    </div>
-  );
-}
-
-export default TitleBar;
--- music-manager/client/music-manager/src/types/SDCardInfo.ts
+++
@@ -1,7 +0,0 @@
-export interface SDCardInfo {
-  path: string;
-  total_bytes: number;
-  free_bytes: number;
-}
-
-
